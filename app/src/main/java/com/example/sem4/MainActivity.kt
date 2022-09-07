270d856a31bd716b36fd6d0e35f35b816ee9f2fe
Williamz21 <u201816224@upc.edue.pe>
2022-09-07 15:24:11 -0500

    second commit

--- app/src/main/java/com/example/sem4/MainActivity.kt
+++ app/src/main/java/com/example/sem4/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.sem4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var number: Int = 0
+    lateinit var questions: ArrayList<Question>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadQuestion()
+        setupBuild()
+    }
+
+    private fun loadQuestion() {
+        questions= ArrayList()
+        questions.add(Question("Es Lima la capital de Perú?",true))
+        questions.add(Question("Es Lima la capital de Chile?",false))
+        questions.add(Question("Es Lima la capital de Brasil?",false))
+        questions.add(Question("Es Buenos Aires la capital de Brasil?",false))
+        questions.add(Question("Es Bogota la capital de Colombia?",false))
+    }
+
+    private fun setupBuild(){
+        val questionShow= findViewById<TextView>(R.id.tvQuestion)
+        val btYes = findViewById<Button>(R.id.btSi)
+        val btNo = findViewById<Button>(R.id.btNo)
+        val btNext=findViewById<Button>(R.id.btNext)
+
+        questionShow.text = questions[number].sentence
+
+        btYes.setOnClickListener {
+            if (questions[number].answer) {
+                Toast.makeText(this,"Correcto",Toast.LENGTH_LONG).show()
+            }
+            else Toast.makeText(this,"Incorrecto",Toast.LENGTH_LONG).show()
+        }
+
+        btNo.setOnClickListener {
+            if (questions[number].answer) {
+                Toast.makeText(this,"Correcto",Toast.LENGTH_LONG).show()
+            }
+            else Toast.makeText(this,"Incorrecto",Toast.LENGTH_LONG).show()
+        }
+
+        btNext.setOnClickListener {
+            number++
+            if(number>questions.size) number = 0
+        }
+
     }
 }
